ac55cba1856616bd51eb1d4be267b201d10ca3a8
Leonardo Cserny <leo.cserny@gmail.com>
2024-06-06 23:35:13 +0300

    removes zx

--- src/bin/locdoc.js
+++ src/bin/locdoc.js
@@ -1,79 +1,103 @@
 #!/usr/bin/env node
-import 'zx/globals'
+
 import * as argparse from "argparse";
 import * as tracer from "tracer";
-import { ManifestParser } from '../manifest.js';
+import {ManifestParser} from '../manifest.js';
+import * as path from "node:path";
+import * as os from "node:os";
+import * as fs from "node:fs/promises";
+import simpleGit from "simple-git";
+import * as child_process from "node:child_process";
+import * as util from "node:util";
+import {Docker} from "docker-cli-js";
 
 const parser = new argparse.ArgumentParser({
     description: "Local Docker Deployer"
 });
-parser.add_argument("-m", "--manifest", { required: true, help: "path to deployment manifest file" });
+parser.add_argument("-m", "--manifest", {required: true, help: "path to deployment manifest file"});
 const args = parser.parse_args();
 
+const docker = new Docker({echo: false});
+const exec = util.promisify(child_process.exec);
+const git = simpleGit();
 const logger = tracer.colorConsole();
 
 try {
-    const manifestParser = new ManifestParser(logger);
-    const manifest = await manifestParser.parse(args.manifest); 
+    const manifestParser = new ManifestParser(logger, getRandomNumberAsString(10000, 99999));
+    const manifest = await manifestParser.parse(args.manifest);
 
-    const workDir = await $`echo -n /tmp/$RANDOM`;
+    const workDir = path.join(os.tmpdir(), getRandomNumberAsString(10000, 99999))
+    logger.info(`Creating workdir '${workDir}'`);
 
-    const artifactRepoDir = await $`echo -n ${workDir}/${manifest.image.name}`;
-    await $`mkdir -p ${artifactRepoDir}`;
-    await $`cd ${artifactRepoDir} && git clone --branch ${manifest.artifact.tag} ${manifest.artifact.repo} .`;
+    logger.info(`Cloning artifact repo`);
+    const artifactRepoDir = path.join(workDir, manifest.image.name);
+    await fs.mkdir(artifactRepoDir, {recursive: true});
+    await git.clone(manifest.artifact.repo, artifactRepoDir, {"--branch": manifest.artifact.tag});
 
-    const configRepoDir = `${artifactRepoDir}/${manifest.config.destinationPath}`;
-    await $`mkdir -p ${configRepoDir}`;
-    const tmpConfigRepoDir = await $`echo -n ${workDir}/$RANDOM`;
-    await $`mkdir -p ${tmpConfigRepoDir}`;
-    await $`cd ${tmpConfigRepoDir} && git clone --branch ${manifest.config.tag} ${manifest.config.repo} .`;
-    await $`mv ${tmpConfigRepoDir}/* ${configRepoDir}`;
+    const configRepoDir = path.join(artifactRepoDir, manifest.config.destinationPath);
+    await fs.mkdir(configRepoDir, {recursive: true});
 
-    await $`cd ${artifactRepoDir} && bash -c ${manifest.artifact.buildCmd}`;
+    logger.info("Cloning config repo");
+    const tmpConfigRepoDir = path.join(workDir, getRandomNumberAsString(10000, 99999));
+    await fs.mkdir(tmpConfigRepoDir, {recursive: true});
+    await git.clone(manifest.config.repo, tmpConfigRepoDir, {"--branch": manifest.config.tag});
 
+    logger.info("Merging config in artifact");
+    await fs.readdir(tmpConfigRepoDir).then(files => {
+        files.forEach((file) => {
+            fs.rename(path.join(tmpConfigRepoDir, file), path.join(configRepoDir, file));
+        });
+    });
+
+    logger.info("Executing build command");
+    process.chdir(artifactRepoDir);
+    await exec(`bash -c '${manifest.artifact.buildCmd}'`);
+
+    logger.info("Building Docker image");
     const dockerImage = `${manifest.image.name}:${manifest.image.version}`;
-    const dockerFilePath = path.join(artifactRepoDir.toString(), manifest.artifact.dockerFile);
-    // TODO: if same image with tag exists, get its ID, at the end delete it
-    await $`docker build -t ${dockerImage} -f ${dockerFilePath} ${artifactRepoDir}`;
+    const dockerFilePath = path.join(artifactRepoDir, manifest.artifact.dockerFile);
+    await docker.command(`build -t ${dockerImage} -f ${dockerFilePath} ${artifactRepoDir}`);
 
-    const networkName = manifest.deploy.network;
-    if (networkName) {
-        const dockerNetResp = await $`docker network ls --filter name=${networkName} --format {{.Name}}`;
-        if (!dockerNetResp.stdout) {
-            await $`docker network create ${networkName}`;
+    const dockerNet = manifest.deploy.network;
+    if (dockerNet) {
+        const networksResp = await docker.command(`network ls --filter name=${dockerNet}`);
+        if (networksResp.network.length === 0) {
+            logger.info(`Docker network '${dockerNet}' not found, creating...`);
+            await docker.command(`network create ${dockerNet}`);
         }
     }
 
-    const containerName = manifest.deploy.name;
-    const dockerContResp = await $`docker container ls -a --filter name=${containerName} --format {{.ID}}:{{.State}}`;
-    if (dockerContResp.stdout) {
-        const split = dockerContResp.toString().split(":");
-        const containerId = split[0];
-        const containerState = split[1].replace(/(\r\n|\n|\r)/gm,"");
-        if (containerState === "running") {
-            await $`docker container stop ${containerId}`;
+    const dockerContainer = manifest.deploy.name;
+    const containersResp = await docker.command(`ps -a --filter name=${dockerContainer}`);
+    if (containersResp.containerList.length > 0) {
+        const container = containersResp.containerList[0];
+        const containerId = container["container id"];
+        logger.info(`Existing container found '${containerId}'`);
+        const containerStatus = container.status;
+        if (containerStatus.toLowerCase().includes("up")) {
+            logger.info(`Stopping container '${dockerContainer}'`);
+            await docker.command(`stop ${dockerContainer}`);
         }
-        await $`docker container rm ${containerId}`;
+        logger.info(`Removing existing container '${dockerContainer}'`);
+        await docker.command(`rm -v ${dockerContainer}`);
     }
 
     let runFlags = manifest.deploy.runFlags;
     if (!runFlags.includes("--network")) {
-        runFlags += " --network " + manifest.deploy.network;
+        runFlags += ` --network ${dockerNet}`;
     }
+    logger.info(`Starting new docker container '${dockerContainer}'`);
+    await docker.command(`run -d ${runFlags} --name ${dockerContainer} ${dockerImage}`);
 
-    const dockerRunArgs = ['-d', ...runFlags.split(' '), '--name', containerName, manifest.image.name + ':' + manifest.image.version];
-    await $`docker run ${dockerRunArgs}`;
+    await docker.command(`builder prune -f`);
 
-    await $`docker builder prune -f`;
-
-    await $`rm -rf ${workDir}`;
+    await fs.rm(workDir, {recursive: true});
 
     logger.info("Done!");
-} catch (error) {
-    let cause = "<unavailable>";
-    if (error.cause) {
-        cause = `${error.cause}`;
-    }
-    // logger.error(`${error.message}\n\tCause: ${cause}`);
-    logger.error(`${error.message}`);
+} catch (e) {
+    logger.error(e);
+}
+
+function getRandomNumberAsString(min, max) {
+    return Math.floor(Math.random() * (max - min) + min).toString();
 }
--- src/manifest.js
+++ src/manifest.js
@@ -1,21 +1,24 @@
-import * as fs from "fs/promises";
+import * as fs from "node:fs/promises";
 import YAML from "yaml";
-import 'zx/globals'
+import lodash from "lodash-es";
 
 export class ManifestParser {
-    constructor(logger) {
+    constructor(logger, randomName) {
         this.logger = logger;
+        this.randomName = randomName;
     }
 
     async parse(manifestFilePath) {
         this.logger.info(`Parsing manifest '${manifestFilePath}'`);
 
         const manifestFile = await fs.readFile(manifestFilePath, "utf8");
-        const randomName = await $`echo -n $RANDOM`;        
-        const template = new Manifest(randomName);
+        const template = new Manifest(this.randomName);
         const manifest = YAML.parse(manifestFile);
-        const mergedManifest = { ...template, ...manifest };
+
+        const mergedManifest = lodash.merge(template, manifest);
         this.validate(mergedManifest);
+
+        this.logger.info(`Manifest data: ${JSON.stringify(mergedManifest)}`);
 
         return mergedManifest;
     }
